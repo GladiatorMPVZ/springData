4d02357088d38d11daa3faedb08b7582893d5e2a
GladiatorMPVZ <inform.work981@mail.ru>
2022-11-29 21:43:17 +0300

    1

---
+++ src/main/java/com/example/springdata/Product.java
@@ -0,0 +1,17 @@
+package com.example.springdata;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+}
